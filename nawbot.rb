74184081180023494f09d4292b4620e05bed80f6
Matthew Schoolmaster <msch@2ohm.com>
2012-09-12 14:50:39 -0700

    nawbot learns some more tricks.  look up xbl status by gamertag

--- nawbot.rb
+++ nawbot.rb
@@ -11,7 +11,7 @@ class Preclick
   listen_to :channel  
 
   def listen(m)
-    URI.extract(m.message) do |url|
+    URI.extract(m.message, "http") do |url|
       case url
         when /youtube\.com/
           resp = self.class.get(url) 
@@ -35,19 +35,60 @@ class Preclick
   end
 end
 
+class XboxLive
+  include Cinch::Plugin
+  include HTTParty
+  
+  match /^!live (.+)/, use_prefix: false
+
+  def xboxlive(gamertag)
+    url = "http://www.xboxleaders.com/api/profile/#{URI.escape(gamertag)}.json"
+    resp = self.class.get(url)
+    if resp.parsed_response['Stat'] == 'ok'
+      status = resp.parsed_response['Data']['OnlineStatus'] rescue nil
+      unless status.nil?
+        "#{gamertag}: #{status}"
+      end
+    else
+      "#{gamertag} status: Probably jerking it..."
+    end
+  end
+
+  def execute(m, gamertag)
+    m.reply CGI.unescapeHTML xboxlive(gamertag)
+  end
+  
+end
+
 nawbot = Cinch::Bot.new do
   configure do |c|
     c.nick = "nawbot"
+    c.password = "nawbotpass*123"
     c.server = "irc.freenode.org"
     c.channels = ["#reddit-naw","#nawbot-test"]
     #c.channels = ["#nawbot-test"]
-    c.plugins.plugins = [Preclick]
+    c.plugins.plugins = [Preclick, XboxLive]
   end
 
   on :message, "hello" do |m|
     m.reply "Hello, #{m.user.nick}"
   end
   
+  on :message, "hello nawbot" do |m|
+    m.reply "Hello, #{m.user.nick}.  Very nice of you to think of me.  I was starting to feel neglected."
+  end
+
+  on :message, "lol" do |m|
+    if 1 == rand(5)
+      m.reply "lol indeed, #{m.user.nick}"
+    end
+  end
+
+  on :message, "LOL" do |m|
+    if 1 == rand(53)
+      m.reply "L 0 freaking L"
+    end
+  end
 end
 
 nawbot.start
